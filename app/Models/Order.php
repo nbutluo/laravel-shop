f80652b50ae78fd22e55f681086594412bb92ce8
nbutluo <nbutluo@163.com>
2021-09-17 19:29:09 +0800

    管理后台 - 优惠券列表

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -96,7 +96,7 @@ class Order extends Model
         $prefix = date('YmdHis');
         for ($i = 0; $i < 10; ++$i) {
             // 随机生成 6 位的数字
-            $no = $prefix.str_pad(random_int(0, 999999), 6, '0', STR_PAD_LEFT);
+            $no = $prefix . str_pad(random_int(0, 999999), 6, '0', STR_PAD_LEFT);
             // 判断是否已经存在
             if (!static::query()->where('no', $no)->exists()) {
                 return $no;
@@ -116,5 +116,10 @@ class Order extends Model
         } while (self::query()->where('refund_no', $no)->exists());
 
         return $no;
+    }
+
+    public function couponCode()
+    {
+        return $this->belongsTo(CouponCode::class);
     }
 }
